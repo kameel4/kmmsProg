bc41b3d81f4af21bd4102dbfef1321d18caca73e
kameel4 <kamilshaimiev@yandex.ru>
2025-08-08 22:56:54 +0300

    the first task from the section on associative containers, 06_was_there_the_number_before

---
+++ yandex_data_structure/06_was_there_the_number_before/06_was_there_the_number_before.cpp
@@ -0,0 +1,46 @@
+/*
+На вход подаётся последовательность целых чисел.
+Для каждого числа выведите в отдельной строке слово YES,
+если это число ранее встречалось в последовательности, и NO, если не встречалось.
+
+Формат ввода
+Вводится список чисел. Все числа списка находятся на одной строке и разделены пробелом.
+Каждое число представимо типом int.
+
+Формат вывода
+Выведите ответ на задачу.
+*/
+
+#include <fstream>
+#include <iostream>
+#include <stdexcept>
+#include <string>
+#include <unordered_set>
+
+int main(){
+    std::unordered_set<int> numbers;
+
+    try{
+        std::ifstream in ("input.txt");
+
+        if (!in.is_open()){
+            throw std::runtime_error("Файл входных данных input.txt отсутствует!");
+        }
+
+        int current_number = 0;
+        std::ofstream out("output.txt");
+        while (in >> current_number){
+            if (numbers.contains(current_number)){
+                out<< "YES";
+            }else{
+                out<< "NO";
+                numbers.insert(current_number);
+            }
+        }
+
+        std::cout<< "Задача выполнена. Смотри результат в файле 04_output.txt" << std::endl;
+    } catch (const std::exception& e) {
+        std::cerr << e.what() << std::endl;
+    }
+
+}
