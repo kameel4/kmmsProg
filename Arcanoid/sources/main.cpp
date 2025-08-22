1a9063c2c07cae41eb970c80c8776320fcf037f6
kameel4 <kamilshaimiev@yandex.ru>
2025-08-23 02:45:33 +0300

    [Arcanoid] Created struct for racket. Init() & put() methods for racket.  (put - in terms of "placing" racket on playing area)

--- Arcanoid/sources/main.cpp
+++ Arcanoid/sources/main.cpp
@@ -1,33 +1,68 @@
-#define WIDTH 65
-#define HEIGHT 25
+#define AREA_WIDTH 65
+#define AREA_HEIGHT 25
 
 #include <algorithm>
 #include <iostream>
 #include <string>
 
+void init();
+void init_racket();
+void put_racket();
+void show();
+struct Tracket{
+    int x, y;
+    int width;
+};
+
 // + 1 - это выделение места для символа конца строки
-char playing_area[HEIGHT][WIDTH + 1];
+char playing_area[AREA_HEIGHT][AREA_WIDTH + 1];
+Tracket racket;
+
+int main(){
+    init();
+
+    init_racket();
+    put_racket();
+
+    show();
+}
 
 void init(){
-    for (int i = 0; i < WIDTH; i++){
+    for (int i = 0; i < AREA_WIDTH; i++){
         playing_area[0][i] = '#';
 
         // искусственно добавляем символ конца строки для корректного вывода
-        playing_area[0][WIDTH] = '\0';
+        playing_area[0][AREA_WIDTH] = '\0';
     }
 
-    for (int i = 1; i < HEIGHT; i++){
-        std::copy(playing_area[0], playing_area[0] + WIDTH + 1, playing_area[i]);
+    std::copy(playing_area[0], playing_area[0] + AREA_WIDTH + 1, playing_area[1]);
+
+    for (int i = 1; i < AREA_WIDTH - 1; i++){
+        playing_area[1][i] = ' ';
+    }
+
+    for (int i = 2; i < AREA_HEIGHT; i++){
+        std::copy(playing_area[1], playing_area[1] + AREA_WIDTH + 1, playing_area[i]);
+    }
+}
+
+void init_racket(){
+    racket.width = 7;
+
+    // размещаем ракетку в центре поля, в самом низу
+    racket.x = (AREA_WIDTH - racket.width) / 2;
+    racket.y = AREA_HEIGHT - 1;
+}
+
+void put_racket(){
+    for (int i = racket.x; i < racket.x + racket.width; i++){
+        playing_area[racket.y][i] = '@';
     }
 }
 
 void show(){
-    for (int i = 0; i < HEIGHT; i++){
+    for (int i = 0; i < AREA_HEIGHT; i++){
         std::cout << playing_area[i] << std::endl;
     }
 }
 
-int main(){
-    init();
-    show();
-}
